b2fbe47e9be7b4c02c37bb242f324eb6d9fa20bc
FedericoGre <federico.garea@gmail.com>
2023-03-03 19:59:37 -0300

    estados CatPet

--- js/rendered.js
+++ js/rendered.js
@@ -51,20 +51,82 @@ function renderAllListArrayToCard(listArrayObject) {
     })
 }
 
+function generarNumeroAleatorio(min,max){
+    return Math.floor(Math.random() * (max - min)) + min;
 
-function catPet(){
-const catPet=document.getElementById("catPet");
-const globoCats=document.getElementById("globoCats");
-catPet.addEventListener("click",()=>{
-    globoCats.classList.toggle("globoCat2");
-    setTimeout( function(){
-        globoCats.classList.toggle("globoCat2")
-    },2000)
-})
+    //no incluye el valor maximo si es entre 1 y 2 tengo que poner min 1 max 3
 }
 
 
-catPet();
+const catPets=document.getElementById("catPet");
+
+function catPet(){
+
+
+const globoCats=document.getElementById("globoCats");
+
+const texts= document.createElement("span");
+
+texts.classList.add(`TextCat`)
+
+
+
+const random= generarNumeroAleatorio(1,6);
+
+console.log(random)
+
+if(random === 1){
+
+    texts.innerText=`Meow!`
+   
+    
+    
+}if(random === 2){
+
+    texts.innerText=`Purr...`
+    
+    
+}
+
+if(random === 3){
+
+    texts.innerText=`😻`
+    
+    
+}
+if(random === 4){
+
+    texts.innerText=`✊😸`
+    
+    
+}
+
+if(random === 5){
+
+    texts.innerText=`❤️`
+    
+    
+}
+
+    globoCats.appendChild(texts);
+
+    globoCats.classList.toggle("globoCat2");
+    setTimeout( function(){
+        globoCats.classList.toggle("globoCat2")
+        texts.remove();
+        
+    },1000)
+
+   
+}
+
+
+
+
+catPets.addEventListener("click",()=>{
+    catPet();  
+})
+
 
 
 
